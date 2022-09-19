f2c9d8c379144a97f323550ee11f72491054ee0d
alexandr <alex98saratovtsev@gmail.com>
2022-09-19 14:17:08 +0400

    Add method CreateArray

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,17 @@
 ﻿System.Console.Write("Введите количество элементов массива (целое положительное число): ");
 int size = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write($"Строка № {i + 1}: ");
+        array[i] = Console.ReadLine();
+    }
+    
+    return array;
+}
+
+string[] arr = CreateArray(size);
