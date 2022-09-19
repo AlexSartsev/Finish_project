51a8daec566116184f534d1926240f02a98cfb36
alexandr <alex98saratovtsev@gmail.com>
2022-09-19 14:20:23 +0400

    Add method PrintArray

--- program/Program.cs
+++ program/Program.cs
@@ -14,4 +14,16 @@ string[] CreateArray(int size)
     return array;
 }
 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
 string[] arr = CreateArray(size);
+PrintArray(arr);
