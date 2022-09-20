abd3a43d7131a2d3a268505751e6eaf10e219ce8
alexandr <alex98saratovtsev@gmail.com>
2022-09-20 21:18:33 +0400

    Add method NumberOfCharacters

--- program/Program.cs
+++ program/Program.cs
@@ -1,8 +1,8 @@
-﻿System.Console.Write("Введите количество элементов массива (целое положительное число): ");
-int size = Convert.ToInt32(Console.ReadLine());
-
-string[] CreateArray(int size)
+﻿string[] CreateArray()
 {
+    System.Console.Write("Введите количество элементов массива (целое положительное число): ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
     string[] array = new string[size];
 
     for (int i = 0; i < size; i++)
@@ -25,21 +25,24 @@ void PrintArray(string[] array)
     Console.Write("]");
 }
 
-string[] NumberOfCharacters (string[] array)
+string[] NumberOfCharacters(string[] array)
 {
+    int count = default;
     string[] arrayTwo = new string[array.Length];
 
-    for (int i = 0; i < arrayTwo.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            arrayTwo[i] = array[i];
+            arrayTwo[count] = array[i];
+            count++;
         }
     }
     return arrayTwo;
 }
 
-string[] arr = CreateArray(size);
-PrintArray(arr);
+string[] arr = CreateArray();
 string[] arr2 = NumberOfCharacters(arr);
+PrintArray(arr);
+Console.Write(" -> ");
 PrintArray(arr2);
